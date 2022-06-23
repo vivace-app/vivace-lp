2e35667ec923e7650998be922b284acdf39f28ec
shu00011 <koha.koishi.gnu.16@gmail.com>
2022-06-23 14:50:19 +0900

    fix: Hero Areaの原型を実装．

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,19 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
+      <div className="min-h-screen hero bg-base-200">
+        <div className="text-center hero-content">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">VIVACE</h1>
+            <button className="m-10 btn btn-primary">
+              Download on the App Store
+            </button>
+            <button className="m10 btn btn-primary">
+              Get IT ON Google Play
+            </button>
+          </div>
+        </div>
+      </div>
       <Component {...pageProps} />
     </>
   )
